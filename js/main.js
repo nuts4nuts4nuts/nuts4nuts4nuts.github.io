b87cf4ee06d86e8768f2cb39c6b8ad44a7ab7b33
David <dvdjoh@gmail.com>
2017-04-03 23:10:50 -0400

    Make portfolio main page

--- js/main.js
+++ js/main.js
@@ -1,42 +1,47 @@
 $(document).ready( function() {
 
-	// Logo
-	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
+  // Logo
+  var $logo 	= $('#logo');
+  if (location.href.indexOf("#") != -1)
+  {
+    if(location.href.substr(location.href.indexOf("#"))!='#about')
+    {
+      $logo.show();
     }
+  }
 
-	// Show logo
-	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('fast');
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('fast');
-	});
-function animMeter(){
+  // Show logo
+  $('#tab-container .tab a').click(function() {
+    $logo.slideDown('fast');
+  });
+
+  // Hide logo
+  $('#tab-about').click(function() {
+    $logo.slideUp('fast');
+  });
+
+  function animMeter() {
     $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
-
-      $('#tab-container').easytabs({
-        animate			: true,
-        updateHash		: true,
-        transitionIn	: 'slideDown',
-        transitionOut	: 'slideUp',
-        animationSpeed	: 400,
-        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
-            if($targetPanel.selector=='#resume'){
-                    animMeter();
-            }
-        });
+      $(this)
+        .data("origWidth", $(this).width())
+        .width(0)
+        .animate({
+          width: $(this).data("origWidth")
+        }, 1200);
     });
+  }
+  animMeter();
+
+  $('#tab-container').easytabs({
+    animate			: true,
+    updateHash		: true,
+    transitionIn	: 'slideDown',
+    transitionOut	: 'slideUp',
+    animationSpeed	: 500,
+    tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
+    if($targetPanel.selector=='#resume')
+    {
+      animMeter();
+    }
+  });
+});
