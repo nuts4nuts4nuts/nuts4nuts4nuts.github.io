339fd4cfcee1a8d90f3ebedf9857b6ca7ca669e2
nuts4nuts4nuts <nuts4nuts4nuts@gmail.com>
2022-04-20 23:07:17 -0400

    Update dotfetch.sh
    Add $

--- dotfetch.sh
+++ dotfetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s expand_aliases
 git clone --bare https://github.com/nuts4nuts4nuts/dotfiles.git $HOME/dotfiles.git
-alias conf="git --git-dir=%HOME/dotfiles.git/ --work-tree=$HOME"
+alias conf="git --git-dir=$HOME/dotfiles.git/ --work-tree=$HOME"
 conf config --local status.showUntrackedFiles no
 conf checkout
